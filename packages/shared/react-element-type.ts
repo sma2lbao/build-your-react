478ec63f8c727f9221556865cf216a0c3cea5687
sma2lbao <sma2lbao@gmail.com>
2024-07-23 14:40:58 +0800

    feat: 初始化react项目

---
+++ packages/shared/react-element-type.ts
@@ -0,0 +1,7 @@
+export type ReactElement = {
+  $$typeof: any;
+  type: any;
+  key: any;
+  ref: any;
+  props: any;
+};
---
+++ packages/shared/react-types.ts
@@ -0,0 +1,9 @@
+export type ReactNode = React$Element<any> | ReactText;
+
+export type ReactEmpty = null | void | boolean;
+
+export type ReactFragment = ReactEmpty | Iterable<React$Node>;
+
+export type ReactNodeList = ReactEmpty | React$Node;
+
+export type ReactText = string | number;
